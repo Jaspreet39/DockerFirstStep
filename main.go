65e64bb80f2fa063685cb630b35f95b0f10917dd
jaspreet39 <jassingh.0244@gmail.com>
2023-09-28 19:44:07 -0400

    Add: Authenticator in main file

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-// import ("fmt")
-import "log"
+import (
+	"fmt"
+)
 
 func main() {
-	log.Println("hello world!")
+	authenticator()
+}
+
+// Japsreet's function
+func authenticator() {
+	var userID int = 500224473
+	var password int = 123456
+	var username int
+	var pass int
+
+	fmt.Println("Please enter the username:")
+	fmt.Scanln(&username)
+
+	fmt.Println("Please enter the password:")
+	fmt.Scanln(&pass)
+
+	if username == userID && password == pass {
+		fmt.Println("you are successfully loggedIn")
+	} else {
+		fmt.Println("Incorrect credentials")
+	}
 }
